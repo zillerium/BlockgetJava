5d8637243198b204aa75bd640e0eef7922c02903
trevor oakley <trevoroakley200@gmail.com>
2019-07-25 04:31:00 +0100

    init

---
+++ src/main/java/BlockgetStart.java
@@ -0,0 +1,10 @@
+public class BlockgetStart {
+
+
+  public static void main(String[] args) {
+      BlockgetAccount anAccount = new BlockgetAccount();
+      anAccount.getAccountByName("trevor3");
+      System.out.println("hellow from blockget");
+  }
+
+}
